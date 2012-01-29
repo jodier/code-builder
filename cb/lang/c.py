13ffc10285b527d1e54446375138c56dd990fc68
Jérôme ODIER <odier.jerome@gmail.com>
2012-01-29 15:15:04 +0100

    =update

--- cb/lang/c.py
+++ cb/lang/c.py
@@ -250,7 +250,8 @@ def generate_global_methods(ctx):
 	print('const char *%s_getExtName(%s_profiles_t, %s_extentions_t);' % (name, name, name))
 	print('')
 	print('bool %s_checkMet(%s_profiles_t, %s_methods_t);' % (name, name, name))
-	print('const char *%s_getMetName(%s_profiles_t, %s_extentions_t);' % (name, name, name))
+	print('const char *%s_getMetName(%s_profiles_t, %s_methods_t);' % (name, name, name))
+	print('')
 	print('void *%s_getMetAddr(%s_profiles_t, %s_methods_t);' % (name, name, name))
 
 	print('')
