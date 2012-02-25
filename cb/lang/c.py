ff371cd37f892108d85d0f923ddbc80dce19df66
Jérôme ODIER <odier.jerome@gmail.com>
2012-02-25 15:01:47 +0100

    =update

--- cb/lang/c.py
+++ cb/lang/c.py
@@ -305,7 +305,7 @@ def emit_impTypes(types, fp):
 
 			elif re2.search(t['from']):
 				cb.utils.printf(fp, 'typedef %s %s%s;' % (
-					re2.sub('', s).strip(), t['name'],
+					re2.sub('', t['from']).strip(), t['name'],
 					''.join(['[%s]' % dim for dim in re2.findall(s)])
 				))
 			else:
